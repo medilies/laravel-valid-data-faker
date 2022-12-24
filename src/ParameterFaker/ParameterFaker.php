e1a28daad31b7aad88397d0a05e412650fa55da2
medilies <medilies@gmail.com>
2022-12-24 17:39:33 +0100

    ParameterFaker::$rules = snake_case parsed rules

--- src/ParameterFaker/ParameterFaker.php
+++ src/ParameterFaker/ParameterFaker.php
@@ -6,13 +6,18 @@ use Illuminate\Validation\ValidationRuleParser;
 
 abstract class ParameterFaker
 {
+    protected array $parsed_rules;
+
     protected array $rules;
 
     public function __construct(
         protected string $param_name,
         array $rules,
     ) {
-        $this->rules = array_map(ValidationRuleParser::class.'::parse', $rules);
+        $this->parsed_rules = array_map(ValidationRuleParser::class.'::parse', $rules);
+
+        // pascal case to snake case
+        $this->rules = array_map(fn ($parsed_rule) => strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $parsed_rule[0])), $this->parsed_rules);
     }
 
     abstract public function generate();
